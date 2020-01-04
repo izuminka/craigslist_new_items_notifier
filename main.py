148382fe74a2773c487bfc8fbe20c4e2dd35651d
izuminka <dolin@ucsb.edu>
2020-01-03 22:44:05 -0800

    init fun for log, query dt, put the query in the loop

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from craigslist import CraigslistForSale
-from datetime import datetime
-import time
-import smtplib
-from os import path
+from craigslist import CraigslistForSale # to get info from cl
+from datetime import datetime # to update the last query dt
+import time # to sleep btw the queries
+import smtplib # to send the email
+from os import path #to check if the log exists in the dir
 
 def conv_date_time(dt):
     # ex convert '2020-01-01 00:00' to 202001010000
@@ -30,12 +30,21 @@ def send_email(email_adr, psw, subj, msg):
     server.login(email, psw)
     server.sendmail(email,email,f'Subject: {subj}\n\n{msg}')
 
+def init(dt_file_name, begin_date_time, query_res_file_name):
+    #initialize the starting query date time
+    with open(dt_file_name, 'w') as f: f.write(begin_date_time)
+
+    #init log.txt if does not exist
+    if not path.exists(query_res_file_name):
+        f= open(query_res_file_name,"w")
+        f.close()
+
 
 # Craigslist query stuff
 my_query = 'dresser'
 min_price = 10
 max_price = 50
-# begin_date_time = '2020-01-01 00:00'
+begin_date_time = '2020-01-01 00:00'
 
 # query times
 sleep_time = 5#60*60 #secs
@@ -50,23 +59,21 @@ email_adr = 'something@gmail.com'
 psw = 'somepassword'
 
 
-# #initialize the starting query date time
-# with open(dt_file_name, 'w') as f: f.write(begin_date_time)
-#
-# #init log.txt if does not exist
 
 
-# CraigslistForSale.show_filters()
-cl_sale = CraigslistForSale(site='santabarbara', category='sss',
-                         filters={'query':my_query,
-                                  'min_price':min_price,
-                                  'max_price': max_price})
-results =  list(cl_sale.get_results(sort_by='newest'))
-
-
+#init the starting query date time and log.txt if does not exist
+init(dt_file_name, begin_date_time, query_res_file_name)
 
 duration = 0 #secs
 while duration < total_duration:
+    # query craigslist
+    cl_sale = CraigslistForSale(site='santabarbara', category='sss',
+                             filters={'query':my_query,
+                                      'min_price':min_price,
+                                      'max_price': max_price})
+                                      # CraigslistForSale.show_filters()
+    results =  list(cl_sale.get_results(sort_by='newest'))
+
     #query the results since the last date time of the query
     dt_query = open(dt_file_name).read().strip() # '2020-01-01 00:00' "%Y-%m-%d %H:%M"
     results_fresh = [d for d in results if isLaterDate(d['datetime'],dt_query)]
