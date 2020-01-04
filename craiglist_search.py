e1eafc4bcc6380e3c6cac97a168e9ddd0deda767
izuminka <dolin@ucsb.edu>
2020-01-03 20:50:50 -0800

    added save last data time of query, for query loop, set duration

--- craiglist_search.py
+++ craiglist_search.py
@@ -1,4 +1,6 @@
 from craigslist import CraigslistForSale
+from datetime import datetime
+import time
 
 def conv_date_time(dt):
     # ex convert '2020-01-01 00:00' to 202001010000
@@ -15,11 +17,25 @@ my_query = 'dresser'
 min_price = 10
 max_price = 50
 
-cl_h = CraigslistForSale(site='santabarbara', category='sss',
+cl_sale = CraigslistForSale(site='santabarbara', category='sss',
                          filters={'query':my_query,
                                   'min_price':min_price,
                                   'max_price': max_price})
-results =  list(cl_h.get_results(sort_by='newest'))
+results =  list(cl_sale.get_results(sort_by='newest'))
 
-date_time_thresh = '2020-01-01 00:00'
-results_fresh = [d for d in results if isLaterDate(d['datetime'],date_time_thresh)]
+
+
+
+dt_file_name = "craigslist_date.txt"
+sleep_time = 60*60 #secs
+thresh_duration = sleep_time*10
+duration = 0 #secs
+while duration < thresh_duration
+    date_time_thresh = open(dt_file_name).read().strip() # 2020-01-01 00:00 "%Y-%m-%d %H:%M"
+
+    results_fresh = [d for d in results if isLaterDate(d['datetime'],date_time_thresh)]
+    #do stuff
+    dt = datetime.now().strftime("%Y-%m-%d %H:%M")
+    with open(dt_file_name, 'w') as f: f.write(dt)
+    duration+=sleep_time
+    time.sleep(sleep_time)
