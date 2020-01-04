812d327e1010324cbb8fb710e6314c9d2e541e43
izuminka <dolin@ucsb.edu>
2020-01-03 22:30:13 -0800

    added email notification, tested, but put dummy email/psw

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from craigslist import CraigslistForSale
 from datetime import datetime
 import time
+import smtplib
+from os import path
 
 def conv_date_time(dt):
     # ex convert '2020-01-01 00:00' to 202001010000
@@ -19,12 +21,42 @@ def results_msg(results_fresh, query_preod):
         msg+=f"{d['name']} {d['price']}\n{d['url']}\n\n"
     return msg+f"{20*'-'}"
 
+def send_email(email_adr, psw, subj, msg):
+    #gmail
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+    server.login(email, psw)
+    server.sendmail(email,email,f'Subject: {subj}\n\n{msg}')
 
-# CraigslistForSale.show_filters()
+
+# Craigslist query stuff
 my_query = 'dresser'
 min_price = 10
 max_price = 50
+# begin_date_time = '2020-01-01 00:00'
 
+# query times
+sleep_time = 5#60*60 #secs
+total_duration = sleep_time*10
+
+# Save info
+query_res_file_name = 'log.txt'
+dt_file_name = "last_query_dt.txt"
+
+#Email info
+email_adr = 'something@gmail.com'
+psw = 'somepassword'
+
+
+# #initialize the starting query date time
+# with open(dt_file_name, 'w') as f: f.write(begin_date_time)
+#
+# #init log.txt if does not exist
+
+
+# CraigslistForSale.show_filters()
 cl_sale = CraigslistForSale(site='santabarbara', category='sss',
                          filters={'query':my_query,
                                   'min_price':min_price,
@@ -32,12 +64,9 @@ cl_sale = CraigslistForSale(site='santabarbara', category='sss',
 results =  list(cl_sale.get_results(sort_by='newest'))
 
 
-query_res_file_name = 'log.txt'
-dt_file_name = "last_query_dt.txt"
-sleep_time = 1#60*60 #secs
-thresh_duration = sleep_time*10
+
 duration = 0 #secs
-while duration < thresh_duration:
+while duration < total_duration:
     #query the results since the last date time of the query
     dt_query = open(dt_file_name).read().strip() # '2020-01-01 00:00' "%Y-%m-%d %H:%M"
     results_fresh = [d for d in results if isLaterDate(d['datetime'],dt_query)]
@@ -47,10 +76,13 @@ while duration < thresh_duration:
     with open(dt_file_name, 'w') as f: f.write(dt_query_new)
 
     #save in human readable format if new query results are non empty
-    msg = results_msg(results_fresh, dt_query+'-->'+dt_query_new)
+    query_period = dt_query+' --> '+dt_query_new
+    msg = results_msg(results_fresh, query_period)
     if msg:
-        # file_name = dt_query.replace(' ','_').replace(':','-') +'--'+dt_query_new.replace(' ','_').replace(':','-')
+        # save the results to log
         with open(query_res_file_name, 'a') as f: f.write(msg)
+        # email the results
+        send_email(email_adr, psw, 'craigslist results', msg)
 
     duration+=sleep_time
     time.sleep(sleep_time)
